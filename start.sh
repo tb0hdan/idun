fb4a2b13fa9fc1070a71942d8613a7500feaf022
Bohdan Turkynewych <tb0hdan@gmail.com>
2020-11-26 03:08:03 +0200

    Startup script

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Typical machine setup: 4G/virtual core, 16 * 4 = 64 containers / 64G RAM
+# Memory limit per process: 2G
+# Memory overcommit ratio: 2
 cpus=$(cat /proc/cpuinfo |grep processor|wc -l)
-cpus=$(( cpus * 2 ))
+cpus=$(( cpus * 4 ))
 
+# Always fetch latest image
+docker pull tb0hdan/idun
+
+# Start 
 docker-compose up -d --scale worker=${cpus}
